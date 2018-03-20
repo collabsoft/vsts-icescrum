bacbf9f379eda65dfa6ab515c3f19bcb34e3e86a
Colin Bontemps <cbontemps@kagilum.com>
2018-03-20 12:05:07 +0100

    update test data set

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
-export INPUT_PROJECTURL="http://localhost:8080/p/PETA"
-export INPUT_ACCESSTOKEN="abcd"
+tsc
+export INPUT_PROJECTURL="http://localhost:8080/icescrum/ws/project/PETA/build/vsts"
+export INPUT_SERVERURL="http://localhost:8080/icescrum"
+export INPUT_PROJECTNAME="PETA"
+export INPUT_ACCESSTOKEN="cee81337d3df43fcb323c141acc8e206"
 node ./notifyIceScrumTSTask/notifyIceScrum.js
