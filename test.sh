43bd33de8ca97c2ee40c0d4ae6cf1099e0c88e2b
Colin Bontemps <cbontemps@kagilum.com>
2018-03-21 14:42:09 +0100

    update test dateset

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ export INPUT_PROJECTNAME="PETA"
 export INPUT_ACCESSTOKEN="cee81337d3df43fcb323c141acc8e206"
 export INPUT_PERSONNALACCESSTOKEN="abh7pnnjm7mzva5b7olv4dsuhcuqleua6avysmgipfc4sn53uy3q" # revoked
 # simulate env variables
-export BUILD_BUILDID="9"
+export BUILD_BUILDID="17"
 export SYSTEM_TEAMPROJECT="MyFirstProject-CI"
 export SYSTEM_TEAMFOUNDATIONCOLLECTIONURI="https://freestylecoco.visualstudio.com/"
 # run task
